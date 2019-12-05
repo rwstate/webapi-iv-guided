8eac7f7a27bd3cc9ba0c52cdd802f807200aad4c
Ryan State <rwstate@gmail.com>
2019-12-05 13:09:20 -0500

    fix dynamic port

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const server = require('./api/server.js');
 
 
 const port = process.env.port || 4000;
-server.listen(4000, () => {
+server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
